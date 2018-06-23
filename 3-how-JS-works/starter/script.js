3a9fb13c7f20e69f2da67e3686d505c4728f0712
Queen Bae <sashakyunghwa@gmail.com>
2018-06-23 13:23:35 -0700

    section three completed

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,8 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// hoisting works up here with function declaration
+calculateAge(1965);
+
+// function declaration
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// hoisting does not work up here for function expression
+// retirement(1965);
+
+// function expression
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+
+// variables 
+// must declare variable before you can use the variable 
+console.log(age);
+var age = 23;
 
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);
 
 
 
@@ -23,7 +50,7 @@
 
 // First scoping example
 
-/*
+// Lexical Scoping (scope chain)
 var a = 'Hello!';
 first();
 
@@ -36,13 +63,13 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -58,20 +85,52 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + d);
 }
-*/
+
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+
+// calculateAge(1985);
+
+// function calculateAge(year) {
+//     console.log(2016 - year);
+//     console.log(this);
+// }
 
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+
+john.calculateAge();
 
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+};
 
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
+// this keyword only becomes something/part of an object as soon as the method/function gets called (very important!)
 
 
